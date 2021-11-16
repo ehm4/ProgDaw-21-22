6d5a1bcb1468d93733581ad8b7fa194c4dbc3fae
Ernesto Hernandez <ernesto.hernandez.fp@iescampanillas.com>
2021-11-16 10:30:04 +0100

    actualizacion

--- Ejercicios libro/tema 7/Ejercicio12.java
+++ Ejercicios libro/tema 7/Ejercicio12.java
@@ -2,5 +2,35 @@ import java.util.Scanner;
 public class Ejercicio12{
   public static void main(String[]args){
     Scanner s = new Scanner(System.in);
+    int [] n=new int [10];
+    int n1=0;
+    int inicial;
+    int ultima;
+
+    for(int i=0;i<10;i++){
+      System.out.println("Introduzca un numero entero: ");
+      n[i]=s.nextInt();
+    }
+    s.close();
+
+    System.out.println("ARRAY inicial");
+    System.out.print("Índice  ");
+    for(int i=0;i<10;i++){
+      System.out.printf("%3d", n1);
+      n1++;
+    }
+    System.out.println("");
+    System.out.print("Valor   ");
+    for(int i=0;i<10;i++){
+      System.out.printf("%3d", n[i]);
+    }
+    do{
+      System.out.println("Introduce el numero inicial: ");
+      inicial=s.nextInt();
+      System.out.println("Introduce el numero final: ");
+      ultima=s.nextInt();
+    }
+    while(inicial>=ultima);
+
   }
 }
